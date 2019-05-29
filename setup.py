e28b4a831216a0fc592bbf054530fa1c28a35323
james <jamesooo@protonmail.com>
2019-05-29 08:40:46 -0700

    adding Click module to requirements for clean startup

--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ import setuptools
 
 setuptools.setup(
     name = 'rest_example-jamesooo',
-    version = '0.2',
+    version = '0.3',
     author = "James O'Neal",
     description = 'An exapmle of a python rest api',
     url = 'https://github.com/jamesooo/rest_example',
     packages = setuptools.find_packages(),
-    install_requires = ['Flask-RESTful==0.3.7'],
+    install_requires = [
+        'Flask-RESTful==0.3.7',
+        'Click==7.0'
+    ],
     classifiers = [
         'Programming Language :: Python :: 3',
         'Operating System :: OS Independent'
